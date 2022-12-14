7a37816d54695739c20af3cf0dd0c586e6e62baf
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-12-13 23:29:31 -0300

    010 add first api controller

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,7 +37,7 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())z
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
